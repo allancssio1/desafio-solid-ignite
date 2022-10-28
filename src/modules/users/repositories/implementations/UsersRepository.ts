c06eabe30b4d564f4592e2712173e8f622f46c12
Allan Cassio <allan.cassio@oqvocequer.com.br>
2022-10-28 16:41:37 -0300

    first commit

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -19,23 +19,43 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+
+    Object.assign(user, {
+      name,
+      email,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    this.users.push(user);
+    return user;
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    return this.users.find((user) => user.id === id);
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    return this.users.find((user) => user.email === email);
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    const indexUser = this.users.indexOf(receivedUser);
+
+    Object.assign(user, {
+      ...receivedUser,
+      admin: true,
+      updated_at: new Date(),
+    });
+    // user.admin = true;
+    // user.updated_at = new Date();
+    this.users[indexUser] = user;
+    return this.users[indexUser];
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,19 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    console.log("entrou no listAll");
+    if (user_id) {
+      console.log("entrou no userId");
+      const userPermited = this.usersRepository.findById(user_id);
+
+      if (!userPermited)
+        throw new Error("error user not found ListAllUsersUseCase");
+
+      if (!userPermited.admin) throw new Error("error ListAllUsersUseCase");
+    }
+    console.log("saindo no listAll");
+    const usersList = this.usersRepository.list();
+    return usersList;
   }
 }
 
